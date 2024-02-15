992419f1f596ef8946ac477a8f6c7e538917fc3e
ArD <ademkhina@list.ru>
2024-02-15 21:34:51 +0700

    initial

---
+++ ofp_1/src/main/scala/Main.scala
@@ -0,0 +1,67 @@
+import scala.annotation.tailrec
+import scala.io.StdIn.readLine
+import scala.math.Pi
+
+
+case class Calcs(xSt: BigDecimal,
+                 xFin: BigDecimal,
+                 dx: BigDecimal,
+                 e: BigDecimal) {
+
+  private def taylorSeriesArctan(x: BigDecimal, precision: BigDecimal): (BigDecimal, Int) = {
+
+    def taylorCurr(n: Int): BigDecimal =
+      BigDecimal(math.pow(-1, n + 1)) / (BigDecimal(2 * n + 1) * x.pow(2 * n + 1))
+
+    @tailrec
+    def taylorSum(n: Int, sum: BigDecimal): (BigDecimal, Int) = {
+      val curr = taylorCurr(n)
+      if (curr.abs < precision) (sum, n)
+      else taylorSum(n + 1, sum + curr)
+    }
+
+    val (sum, n) = taylorSum(0, BigDecimal(0))
+    (Pi / 2 + sum, n)
+  }
+
+  def printTaylorSeriesTable(): Unit = {
+    println("\tx\t\t|\tf(x)\t\t|\tTaylor(x)\t|\tTI")
+    println("-------------------------------------------------")
+    val x = xSt
+    printTaylorSerie(x)
+  }
+
+  @tailrec
+  private def printTaylorSerie(x: BigDecimal): Unit = {
+    val fx = math.atan(x.toDouble)
+    val (taylorX, n) = taylorSeriesArctan(x, e)
+    println("%.4f \t|\t %.4f \t|\t %.4f \t|\t %d".format(x, fx, taylorX, n))
+    if (x < xFin) {
+      printTaylorSerie(x + dx)
+    }
+  }
+}
+
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Введите числа через пробел:")
+    val input = readLine().replace(",", ".").split("\\s+")
+    if (input.length == 4) {
+      try {
+        val numbers = input.map(BigDecimal(_))
+        val Array(xSt, xFin, dx, e) = numbers
+        require(xFin > xSt, "Конечное значение должно быть больше начального значения")
+        require(dx > 0, "Шаг должен быть положительным числом")
+        require(e > 0, "Ошибка должна быть положительным числом")
+        val calcs = Calcs(xSt, xFin, dx, e)
+        calcs.printTaylorSeriesTable()
+      } catch {
+        case e: IllegalArgumentException =>
+          println(s"Error: ${e.getMessage}")
+      }
+    } else {
+      println("Количество чисел должно равняться четырем!")
+    }
+  }
+}
---
+++ ofp_2/src/main/scala/Main.scala
@@ -0,0 +1,24 @@
+import java.io.File
+import scala.io.Source
+import scala.io.StdIn.readLine
+
+object Main {
+  def main(args: Array[String]): Unit = {
+    println("Введите путь к файлу:")
+    val filePath = readLine()
+    try {
+      val file = Source.fromFile(filePath)
+      println("Файл считан")
+      println("Введите количество слов наименьшей длины, которые следует вывести")
+      val num = readLine().toInt
+      val words = file.getLines.flatMap(_.split("\\s+")).filter(_.length > 1).toList
+      val shortestWords = words.sortBy(word => (word.length, word)).take(num)
+      shortestWords.foreach(println)
+    } catch {
+      case e: NumberFormatException =>
+        println("Введеное число не является целым")
+      case e: java.io.FileNotFoundException =>
+        println("Файл по данному путь не найден")
+    }
+  }
+}
