7c84e994d1409906bc0dbd6e3cab16058d0df8d4
ArD <ademkhina@list.ru>
2024-03-15 01:45:23 +0700

    finfin

--- ofp_3/src/main/scala/Main.scala
+++ ofp_3/src/main/scala/Main.scala
@@ -2,75 +2,6 @@ import scala.annotation.tailrec
 import scala.io.StdIn.readLine
 import scala.collection.mutable.ArrayBuffer
 
-case class StringConverter(inp: List[String]){
-  private val len = inp.size
-  private val str = makeString(0, cs = false, "", sh = false)
-
-  @tailrec
-  private def makeString(i: Int, cs: Boolean, prgr: String, sh: Boolean): String = {
-    val ch = inp(i).toUpperCase()
-    if (i == len - 1){
-      if (ch != "CAPSLOCK" && ch != "SHIFT") {
-        return prgr + getSymbol(ch, cs, sh)
-      }
-      return prgr
-    }
-    if (ch == "CAPSLOCK") {
-      makeString(i + 1, !cs, prgr, sh = false)
-    }
-    else if (ch == "SHIFT") {
-      makeString(i + 1, cs, prgr, sh = true)
-    }
-    else {
-      makeString(i + 1, cs, prgr + getSymbol(ch, cs, sh), sh = false)
-    }
-  }
-
-  private def getSymbol(sym: String, cs: Boolean, sh: Boolean): String = {
-    val flag = if (sh) !cs else cs
-    if (flag) {
-      return sym.toUpperCase()
-    }
-    sym.toLowerCase()
-  }
-
-  def getString: String = {
-    str
-  }
-
-  def getAlNum: ArrayBuffer[Char]  = {
-    val symbols = ArrayBuffer[Char]()
-    for (let <- str) {
-      symbols.addOne(let)
-    }
-    symbols
-  }
-
-  def getRaw: ArrayBuffer[String]  = {
-    val symbols = ArrayBuffer[String]()
-    for (let <- inp) {
-      if (let.toUpperCase != "CAPSLOCK" && let.toUpperCase != "SHIFT"){
-        symbols.addOne(let)
-      }
-    }
-    symbols
-  }
-
-  def isCapsLocked: Boolean = {
-    val cs = false
-    def Cl(cs: Boolean, i: Int): Boolean = {
-      val ch = inp(i).toUpperCase()
-      if (i != len - 1) {
-        Cl(if (ch == "CAPSLOCKED") !cs else cs, i + 1)
-      }
-      cs
-    }
-    val res = Cl(cs, 0)
-    println(res)
-    res
-  }
-}
-
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -79,16 +10,14 @@ object Main {
     val test_2 = "ShiFt    h e capSlOck l    l shift O".split("\\s+").toList
     val test_3 = "shift S r i  capslock Jayawa shift r denepura capslock shift K otte".split("\\s+").toList
     val lst = List(test_1, test_2, test_3)
+    val conv = new StringConverter
     for (t <- lst) {
-      val conv = StringConverter(t)
       println("----------------------------")
-      println(conv.getAlNum)
-      println(conv.getRaw)
-      println(conv.getString)
-      conv.isCapsLocked
+      println(conv.getAlNum(t))
+      println(conv.getRaw(t))
+      println(conv.getString(t))
+      conv.isCapsLocked(t)
       println("----------------------------")
-
     }
-
   }
 }
---
+++ ofp_3/src/main/scala/StringConverter.scala
@@ -0,0 +1,72 @@
+import scala.annotation.tailrec
+import scala.collection.mutable.ArrayBuffer
+
+class StringConverter{
+  private val buffer: ArrayBuffer[(Int, String)] = ArrayBuffer().empty
+
+  private def getSymbol(sym: String, cs: Boolean, sh: Boolean): String = {
+    val flag = if (sh) !cs else cs
+    if (flag) {
+      return sym.toUpperCase()
+    }
+    sym.toLowerCase()
+  }
+
+  def getString(inp: List[String]): String = {
+    val len = inp.size
+    @tailrec
+    def makeString(i: Int, cs: Boolean, prgr: String, sh: Boolean): String = {
+      val ch = inp(i).toUpperCase()
+      if (i == len - 1){
+        if (ch != "CAPSLOCK" && ch != "SHIFT") {
+          return prgr + getSymbol(ch, cs, sh)
+        }
+        return prgr
+      }
+      if (ch == "CAPSLOCK") {
+        makeString(i + 1, !cs, prgr, sh = false)
+      }
+      else if (ch == "SHIFT") {
+        makeString(i + 1, cs, prgr, sh = true)
+      }
+      else {
+        makeString(i + 1, cs, prgr + getSymbol(ch, cs, sh), sh = false)
+      }
+    }
+    makeString(0, cs = false, "", sh = false)
+  }
+
+  def getAlNum(inp: List[String]): String = {
+    val str = getString(inp)
+    val symbols = ArrayBuffer[Char]()
+    for (let <- str) {
+      symbols.addOne(let)
+    }
+    String.format("[%s]", symbols.toList.mkString(", "))
+  }
+
+  def getRaw(inp: List[String]): String  = {
+    val symbols = ArrayBuffer[String]()
+    for (let <- inp) {
+      if (let.toUpperCase != "CAPSLOCK" && let.toUpperCase != "SHIFT"){
+        symbols.addOne(let)
+      }
+    }
+    String.format("[%s]", symbols.toList.mkString(", "))
+  }
+
+  def isCapsLocked(inp: List[String]): Boolean = {
+    val len = inp.size
+    val cs = false
+    def Cl(cs: Boolean, i: Int): Boolean = {
+      val ch = inp(i).toUpperCase()
+      if (i != len - 1) {
+        Cl(if (ch == "CAPSLOCKED") !cs else cs, i + 1)
+      }
+      cs
+    }
+    val res = Cl(cs, 0)
+    println(res)
+    res
+  }
+}
